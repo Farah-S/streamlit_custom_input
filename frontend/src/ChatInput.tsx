d3a77b594b8612007ee79b6a1d535615ff628ceb
Farah Sherif <farah.faroha132@gmail.com>
2023-12-08 22:38:07 +0200

    added shift enter

--- frontend/src/ChatInput.tsx
+++ frontend/src/ChatInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState}  from "react";
+import React, { useEffect, useRef, useState}  from "react";
 import {
   withStreamlitConnection, Streamlit, ComponentProps,
 } from "streamlit-component-lib";
@@ -17,23 +17,39 @@ document.body.style.backgroundColor = "transparent";
 const ChatInput=(props:ComponentProps)=>{
   const {initialValue, key}: ChatInputArgs = props.args;
   const [value, setValue] = useState<string>(initialValue);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
   useEffect(() => Streamlit.setFrameHeight());
 
   const handleClick = () => {
-      setValue(value);
-      Streamlit.setComponentValue(value)
+      setValue(value.trim());
+      Streamlit.setComponentValue(value.trim())
       setValue("")
-      // console.log(value)
+      if(inputRef.current){
+        inputRef.current.focus()
+      }
     };
 
+  const onEnterClick = (event:React.KeyboardEvent) => {
+      if (event.key === "Enter" && event.shiftKey==false)
+        handleClick();
+    };
+    
+  const onChangeHandler = ( ev: React.ChangeEvent<HTMLTextAreaElement>, ): void => setValue(ev.target.value)
+
+
   return (
-    <Row style={{ justifyContent:"center", paddingTop:"10px"}}>
-          <TextareaAutosize key={key} placeholder="Type here.." value={value} onChange={(
-                ev: React.ChangeEvent<HTMLTextAreaElement>,
-            ): void => setValue(ev.target.value)} autoFocus style={{resize:"none", marginLeft:"0px", marginRight:"10px",marginTop:"7px", width:"550px", borderRadius:"1rem",boxShadow: "none",maxBlockSize:"75px",borderColor: "transparent",backgroundColor: "#f1faff"}}/>
-      
-        <Button type="submit" onClick={handleClick} style={{width:"55px", borderRadius: "3rem", paddingRight:"5px",paddingTop:"8px", paddingLeft:"9px", display: "flex", justifyContent:"center", backgroundColor: "#f8efff", color: "#5b5b5c",borderColor:"transparent", height:"42px"}} >
+    <Row style={{ justifyContent:"center", paddingTop:"0px", paddingLeft:"90px"}}>
+        <TextareaAutosize key={key} 
+        placeholder="Type here.." 
+        value={value} ref={inputRef} 
+        onKeyUp={onEnterClick} onChange={onChangeHandler} autoFocus 
+        style={{whiteSpace:"pre-line",resize:"none", marginLeft:"10px", marginRight:"10px",marginTop:"1.5px", width:"70%", borderRadius:"1rem",boxShadow: "none",maxBlockSize:"75px",borderColor: "transparent",backgroundColor: "#f1faff"}}/>
+        
+        <Button type="submit" onClick={handleClick} style={{width:"55px", borderRadius: "3rem", paddingRight:"5px",paddingTop:"3px", paddingLeft:"9px", display: "flex", justifyContent:"center", backgroundColor: "#f8efff", color: "#5b5b5c",borderColor:"transparent", height:"33px"}} >
+        
           <Icon baseClassName="material-symbols-outlined">send</Icon>
+        
         </Button>
     </Row>
   );
