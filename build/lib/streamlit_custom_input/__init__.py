4d1d4e1d94be361eacb9872ec43420aed6ebb452
Farah Sherif <farah.faroha132@gmail.com>
2023-12-10 22:50:17 +0200

    published

--- build/lib/streamlit_custom_input/__init__.py
+++ build/lib/streamlit_custom_input/__init__.py
@@ -10,6 +10,7 @@ parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "frontend/build")
 component = components.declare_component("ChatInput", path=build_dir)
 
+
 def ChatInput(key="button", initialValue="") -> str:
     component_value = component(initialValue=initialValue,key=key)
     return component_value
