d7067b00cc41c681d112b3fc29bcefe66000cbdb
Farah Sherif <farah.faroha132@gmail.com>
2024-01-03 01:19:58 +0200

    fixed hook call error

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
 import streamlit as st
-# from streamlit_custom_chat import ChatContainer
 from streamlit_custom_input import ChatInput
-# from streamlit_custom_chat import ChatBubble
-# from custom_chat_input import ChatInput
 import base64
 from key_generator.key_generator import generate
-from streamlit_js_eval import streamlit_js_eval
 
 st.set_page_config(layout="wide")
 
-# titlecol1, titlecol2, titlecol3 = st.columns([15,10,12])
-# with titlecol1:
-#     # st.header("A cat")
-#     # st.image("https://static.streamlit.io/examples/cat.jpg")
-#     st.markdown('#')
-# with titlecol2:
-#     st.title("Simple chat")
-# with titlecol3:
-#     # st.header("A cat")
-#     # st.image("https://static.streamlit.io/examples/cat.jpg")
-#     st.markdown('#')  
-    
 def set_bg_hack(main_bg):
     '''
     A function to unpack an image from root folder and set as bg.
@@ -100,122 +84,23 @@ set_page_container_style()
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
-if "len" not in st.session_state:
-    st.session_state.len = 0
-# ChatContainer(messages=[{"role":"assistant","content":"Hello! How may I help you?"}], key=str(0))
-    
-# ChatContainer(messages=[ {"role":"assistant", "content":"or incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, su"} , 
-#                         {"role":"user", "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempnt in culpa qui officia deserunt mollit anim id est laborum."},
-#                         {"role":"user", "content":"Lorem ipsum dololamco laborur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-#                         {"role":"user", "content":"is nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepte"},
-#                         {"role":"user", "content":"r sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ul"},
-#                         {"role":"assistant", "content":"ation ul"},
-#                         {"role":"assistant", "content":"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"} ])
-
-# style="
-    # background-color: #fffeff;
-    # border-radius: 2rem;
-    # max-width: 900px;
-    # padding-top: 18px;
-    # box-shadow: inset 0px 0 15px 1px rgba(0,0,0,0.05), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12);
-    # justify-self: center;
-    # align-self: center;
-# " 
-# st.sidebar.header("n")
-# st.sidebar.subheader(‘1.please chose which app you want to operate’)
-# with st.container():
-#     CustomChatBubble(role="assistant", content="Hello! How may I help you?", key=str(0))
-#     # Display chat messages from history on app rerun
-#     for message in st.session_state.messages:
-#         CustomChatBubble(role=message["role"], content=message["content"], key=(message["role"]+str(c)))
-#         c+=1
-# ChatContainer(messages=st.session_state.messages, key=str(c))
-
-# prompt=""
-
-
 
     
 if len(st.session_state.messages) == 0:
-    st.session_state.messages.append({"role":"assistant","content":"Hello! How may I help you?","key":0})
-    st.session_state.messages.append({"role":"user","content":"Hello!","key":1})
-    st.session_state.messages.append({"role":"user","content":"Hello!","key":1})
-    st.session_state.messages.append({"role":"assistant","content":"HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello !","key":1})
-    st.session_state.messages.append({"role":"assistant","content":"Hello!","key":1})
-    st.session_state.messages.append({"role":"assistant","content":"Hello!","key":1})
-    st.session_state.messages.append({"role":"assistant","content":"Hello!","key":1})
-    st.session_state.messages.append({"role":"user","content":"Hello!","key":1})
-    st.session_state.messages.append({"role":"assistant","content":"Hello!","key":1})
-    st.session_state.messages.append({"role":"assistant","content":"Hello!","key":1})
-    # CustomChatBubble(role="assistant", content="Hello! How may I help you?")
-    # ChatContainer(messages=st.session_state.messages, key=str(c))
-    # prompt=ChatInput()
-    # st.session_state.len=len(st.session_state.messages)
+    st.session_state.messages.append("Write something!")
+   
 
-#     # Accept user input
-#     if prompt := st.chat_input("What is up?"):
-#         # Display user message in chat message container
-#         CustomChatBubble(role="user", content=prompt, key=("user"+str(c)))
-#         CustomChatBubble(role="assistant", content=prompt, key=("assistant"+str(c)))
-#         c+=1
-#         # Add user message to chat history
-#         st.session_state.messages.append({"role": "user", "content": prompt})
-#         st.session_state.messages.append({"role": "assistant", "content": prompt})
- 
+def refresh(key:str):
+    del st.session_state[key]
+    st.rerun()
 
-col1, col2 = st.columns([2,13]) 
-        
-# for index in st.session_state.messages: 
-    # CustomChatBubble(role=index["role"], content=index["content"], key=(index["role"]+str(c)))
-#     c+=1
-with col1:
-    st.markdown("#")
-    st.markdown("#")
-    st.markdown("#")
-    # st.markdown("#")
-    st.button("Take a Quiz", use_container_width=True)
-with col2:
-    # ChatContainer(messages=st.session_state.messages,key=str(55))
-
-    def refresh(key:str):
-        # st.session_state["ChatInput"] = ""
-        
-        del st.session_state[key]
-        st.rerun()
-
-    # if st.button(label="assistant", key="assistant"):
-    #     key = generate()
-    #     st.session_state.messages.append({"role": "assistant", "content": "whatever","key":"assistant-"+key.get_key()})
-    #     refresh('assistant')
-        
-
-    if prompt:= ChatInput(initialValue="",key="inputButton"):
-        st.session_state.state=True
-        # Display user message in chat message container
-        # CustomChatBubble(role="user", content=prompt, key=("user"+str(c)))
-        # CustomChatBubble(role="assistant", content=prompt, key=("assistant"+str(c)))
-        
-        print(prompt)
-        key = generate()
-        # print(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt,"key":"user-"+key.get_key()})
-        # st.session_state.messages.append({"role": "assistant", "content": prompt,"key":"assistant-"+key.get_key()})
-        
-        # ChatInput()
-        # updateMessages(st.session_state)
-        # del st.session_state['button']
-        # st.rerun()
-        refresh('inputButton')
-        # streamlit_js_eval(js_expressions="parent.window.location.reload()")
-        # Container(messages=st.session_state.messages, key=str(c))
-        # if len(st.session_state.messages)>st.session_state.len:
-        # st.session_state.len=len(st.session_state.messages)
-        # prompt=""
-        # st.markdown('#')
-        # st.markdown('#')
-        # st.markdown('#')
-        # ChatContainer(messages=st.session_state.messages, key=str(55))
-        # prompt = ChatInput(initialValue="")
-        # ChatContainer(messages=st.session_state.messages, key=str(c))
-            
+for i in st.session_state.messages:
+    st.write(i)
+    
+if prompt:= ChatInput(initialValue="",key="inputButton",inputStyle={"backgroundColor":"cyan"}, buttonStyle={"height":"30px"}):
+    
+    key = generate()
+    st.session_state.messages.append(prompt)
+    
+    refresh('inputButton')
+       
--- streamlit_custom_input/frontend/src/ChatInput.tsx
+++ streamlit_custom_input/frontend/src/ChatInput.tsx
@@ -1,8 +1,7 @@
+import React, { useEffect, useRef, useState}  from "react";
 import {
-  StreamlitComponentBase,
-  withStreamlitConnection, Streamlit
+  withStreamlitConnection, Streamlit, ComponentProps,
 } from "streamlit-component-lib";
-import { ReactNode, useEffect, useRef, useState } from "react";
 import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, } from "react-bootstrap";
@@ -34,23 +33,15 @@ interface State {
 
 document.body.style.backgroundColor = "transparent";
 
-/**
- * This is a React-based component template. The `render()` function is called
- * automatically when your component should be re-rendered.
- */
-class ChatInput extends StreamlitComponentBase<State> {
-  
-  public render = (): ReactNode => {
-    
-    const { initialValue, key, inputStyle, buttonStyle} = this.props.args;
+const ChatInput=(props:ComponentProps)=>{
+  const { initialValue, key, inputStyle, buttonStyle} = props.args;
+  // const {initialValue, key}: State = props.args;
+  const [value, setValue] = useState<string>(initialValue);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
 
-    const [value, setValue] = useState<string>(initialValue);
-    
-    const inputRef = useRef<HTMLTextAreaElement>(null);
-    
-    useEffect(() => Streamlit.setFrameHeight());
-    
-    const handleClick = () => {
+  useEffect(() => Streamlit.setFrameHeight());
+
+  const handleClick = () => {
       setValue(value.trim());
       Streamlit.setComponentValue(value.trim())
       setValue("")
@@ -59,7 +50,7 @@ class ChatInput extends StreamlitComponentBase<State> {
       }
     };
 
-    const onEnterClick = (event:React.KeyboardEvent) => {
+  const onEnterClick = (event:React.KeyboardEvent) => {
       if (event.key === "Enter"){
         event.preventDefault()
         if (event.key === "Enter" && event.shiftKey==false){
@@ -70,9 +61,9 @@ class ChatInput extends StreamlitComponentBase<State> {
       }
     };
     
-    const onChangeHandler = ( ev: React.ChangeEvent<HTMLTextAreaElement>, ): void => setValue(ev.target.value)
+  const onChangeHandler = ( ev: React.ChangeEvent<HTMLTextAreaElement>, ): void => setValue(ev.target.value)
 
-    const istyle={
+const istyle={
         paddingLeft: ("paddingLeft" in inputStyle? inputStyle["paddingLeft"]: "10px"), 
         paddingRight: ("paddingRight" in inputStyle? inputStyle["paddingRight"]:"10px"),
         whiteSpace: ("whiteSpace" in inputStyle? inputStyle["whiteSpace"]:"pre-line"), 
@@ -112,12 +103,119 @@ class ChatInput extends StreamlitComponentBase<State> {
         </Button>
       </Row>
     )
-  }
 }
 
-// "withStreamlitConnection" is a wrapper function. It bootstraps the
-// connection between your component and the Streamlit app, and handles
-// passing arguments from Python -> Component.
-//
-// You don't need to edit withStreamlitConnection (but you're welcome to!).
-export default withStreamlitConnection(ChatInput)
+export default withStreamlitConnection(ChatInput);
+
+// interface State {
+//   initialValue: string,
+  
+//   key: "ChatInput",
+
+//   inputStyle:{
+//     paddingLeft:"10px", paddingRight:"10px",
+//     whiteSpace:"pre-line", resize:"none", 
+//     marginLeft:"0", marginRight:"10px",
+//     paddingBottom:"3px", width:"75%", 
+//     borderRadius:"1rem", maxBlockSize:"75px", 
+//     borderColor: "#f4efff", backgroundColor: "#fcf9ff"},
+  
+//     buttonStyle:{
+//       width:"55px", borderRadius: "3rem", 
+//       paddingRight:"5px", paddingTop:"3px", 
+//       paddingLeft:"9px", display: "flex", 
+//       justifyContent:"center", 
+//       backgroundColor: "#edf1ff", color: "#5b5b5c",
+//       height:"33px"
+//     }
+// }
+
+// document.body.style.backgroundColor = "transparent";
+
+// /**
+//  * This is a React-based component template. The `render()` function is called
+//  * automatically when your component should be re-rendered.
+//  */
+// class ChatInput extends StreamlitComponentBase<State> {
+  
+//   public render = (): ReactNode => {
+    
+//     const { initialValue, key, inputStyle, buttonStyle} = this.props.args;
+    
+//     const [value, setValue] = useState<string>(initialValue);
+    
+//     const inputRef = useRef<HTMLTextAreaElement>(null);
+    
+//     useEffect(() => Streamlit.setFrameHeight());
+    
+//     const handleClick = () => {
+//       setValue(value.trim());
+//       Streamlit.setComponentValue(value.trim())
+//       setValue("")
+//       if(inputRef.current){
+//         inputRef.current.focus()
+//       }
+//     };
+
+//     const onEnterClick = (event:React.KeyboardEvent) => {
+//       if (event.key === "Enter"){
+//         event.preventDefault()
+//         if (event.key === "Enter" && event.shiftKey==false){
+//           handleClick();
+//         }else{
+//           setValue(value+'\n')
+//         }
+//       }
+//     };
+    
+//     const onChangeHandler = ( ev: React.ChangeEvent<HTMLTextAreaElement>, ): void => setValue(ev.target.value)
+
+//     const istyle={
+//         paddingLeft: ("paddingLeft" in inputStyle? inputStyle["paddingLeft"]: "10px"), 
+//         paddingRight: ("paddingRight" in inputStyle? inputStyle["paddingRight"]:"10px"),
+//         whiteSpace: ("whiteSpace" in inputStyle? inputStyle["whiteSpace"]:"pre-line"), 
+//         resize: ("resize" in inputStyle? inputStyle["resize"]:"none"), 
+//         marginLeft: ("marginLeft" in inputStyle? inputStyle["marginLeft"]:"0"), 
+//         marginRight: ("marginRight" in inputStyle? inputStyle["marginRight"]:"10px"),
+//         paddingBottom: ("paddingBottom" in inputStyle? inputStyle["paddingBottom"]:"3px"),
+//         width: ("width" in inputStyle? inputStyle["width"]:"75%"), 
+//         borderRadius: ("borderRadius" in inputStyle? inputStyle["borderRadius"]:"1rem"), 
+//         maxBlockSize: ("maxBlockSize" in inputStyle? inputStyle["maxBlockSize"]:"75px"), 
+//         borderColor: ("borderColor" in inputStyle? inputStyle["borderColor"]:"#f4efff"), 
+//         backgroundColor: ("backgroundColor" in inputStyle? inputStyle["backgroundColor"]:"#fcf9ff")
+//     }
+
+//     const bstyle={
+//         width: ("width" in buttonStyle? buttonStyle["width"]:"55px"), 
+//         borderRadius: ("borderRadius" in buttonStyle? buttonStyle["borderRadius"]:"3rem"), 
+//         paddingRight: ("paddingRight" in buttonStyle? buttonStyle["paddingRight"]:"5px"), 
+//         paddingTop: ("paddingTop" in buttonStyle? buttonStyle["paddingTop"]:"3px"), 
+//         paddingLeft: ("paddingLeft" in buttonStyle? buttonStyle["paddingLeft"]:"9px"), 
+//         display: ("display" in buttonStyle? buttonStyle["display"]:"flex"), 
+//         justifyContent: ("justifyContent" in buttonStyle? buttonStyle["justifyContent"]:"center"), 
+//         backgroundColor: ("backgroundColor" in buttonStyle? buttonStyle["backgroundColor"]:"#edf1ff"), 
+//         color: ("color" in buttonStyle? buttonStyle["color"]:"#5b5b5c"),
+//         height: ("height" in buttonStyle? buttonStyle["height"]:"33px")
+//     }
+
+//     return (
+//       <Row style={{ justifyContent:"center", paddingTop:"0px", paddingLeft:"2%"}}>
+//         <TextareaAutosize key={key} 
+//         placeholder="Type here.." 
+//         value={value} ref={inputRef} 
+//         onKeyDown={onEnterClick} onChange={onChangeHandler} autoFocus 
+//         style={{paddingLeft:istyle["paddingLeft"],paddingRight:istyle["paddingRight"],whiteSpace:istyle["whiteSpace"],resize:istyle["resize"], marginLeft:istyle["marginLeft"], marginRight:istyle["marginRight"],paddingBottom:istyle["paddingBottom"], width:istyle["width"], borderRadius:istyle["borderRadius"],boxShadow: "none",maxBlockSize:istyle["maxBlockSize"],borderColor: istyle["borderColor"],backgroundColor: istyle["backgroundColor"]}}/>
+//         <Button type="submit" onClick={handleClick} style={{width:bstyle["width"], borderRadius: bstyle["borderRadius"], paddingRight:bstyle["paddingRight"],paddingTop:bstyle["paddingTop"], paddingLeft:bstyle["paddingLeft"], display: bstyle["display"], justifyContent:bstyle["justifyContent"], backgroundColor: bstyle["backgroundColor"], color: bstyle["color"],borderColor:"transparent", height:bstyle["height"]}} >
+//           <Icon baseClassName="material-symbols-outlined">send</Icon>
+//         </Button>
+//       </Row>
+//     )
+//   }
+// }
+
+// // "withStreamlitConnection" is a wrapper function. It bootstraps the
+// // connection between your component and the Streamlit app, and handles
+// // passing arguments from Python -> Component.
+// //
+// // You don't need to edit withStreamlitConnection (but you're welcome to!).
+// export default withStreamlitConnection(ChatInput)
